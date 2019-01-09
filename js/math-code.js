222959482aa2fae022f16457f262b15c1f845c7d
zhaoyu1 <zhaoyu1@corp.netease.com>
2019-01-09 10:48:30 +0800

    make math-code.js work

--- js/math-code.js
+++ js/math-code.js
@@ -1,22 +1,23 @@
 (function() {
   var i, text, code, codes = document.getElementsByTagName('code');
-  for (i = 0; i < codes.length;) {
+  for (i = 0; i < codes.length; ++i) {
     code = codes[i];
     if (code.parentNode.tagName !== 'PRE' && code.childElementCount === 0) {
       text = code.textContent;
-	  if (/^\$\$[^$]/.test(text) && /[^$]\$\$$/.test(text)) {
+      if (/^\$\$[^$]/.test(text) && /[^$]\$\$$/.test(text)) {
         text = text.replace(/^\$\$/, '\\[').replace(/\$\$$/, '\\]');
+        code.textContent = text;		
       }
-      if (/^\$[^$]/.test(text) && /[^$]\$$/.test(text)) {
+      else if (/^\$[^$]/.test(text) && /[^$]\$$/.test(text)) {
         text = text.replace(/^\$/, '\\(').replace(/\$$/, '\\)');
+        code.textContent = text;
       }
-	  code.textContent = text;
+	  
       if (/^\\\((.|\s)+\\\)$/.test(text) || /^\\\[(.|\s)+\\\]$/.test(text) /*||
           /^\$(.|\s)+\$$/.test(text) ||
           /^\\begin\{([^}]+)\}(.|\s)+\\end\{[^}]+\}$/.test(text)*/) {
         code.outerHTML = code.innerHTML;  // remove <code></code>
       }
     }
-    i++;
   }
 })();
